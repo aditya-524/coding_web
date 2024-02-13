7647325eee1c6c5109100213dd6b197541105f7c
aditya-524 <102483565+aditya-524@users.noreply.github.com>
2024-02-13 08:38:34 +0530

    day8

---
+++ stratascratch/2003/solution.py
@@ -0,0 +1,18 @@
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+loans.head()
+# submissions.head()
+
+loans["created_at"] = pd.to_datetime(loans["created_at"])
+
+result = loans.loc[
+    loans[loans["type"] == "Refinance"]
+    .groupby("user_id")["created_at"]
+    .idxmax()
+]
+result = pd.merge(result, submissions, left_on="id", right_on="loan_id", how="left")
+result = result[["user_id", "balance"]]
+
+result
